0458e6c3dad26bbf63f3e82f95af936f386c69d4
rc2pc2 <petriccarcc@gmail.com>
2025-06-19 19:25:14 +0200

    implemented second part of the assignments

--- src/main/java/org/lessons/java/wdpt6/pizzeria/la_mia_pizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/java/wdpt6/pizzeria/la_mia_pizzeria/controller/PizzaController.java
@@ -1,10 +1,14 @@
 package org.lessons.java.wdpt6.pizzeria.la_mia_pizzeria.controller;
 
+import java.util.Optional;
+
+import org.lessons.java.wdpt6.pizzeria.la_mia_pizzeria.model.Pizza;
 import org.lessons.java.wdpt6.pizzeria.la_mia_pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +22,14 @@ public class PizzaController {
     public String index(Model model){
         model.addAttribute("pizzas", pizzaRepository.findAll());
         return "pizzas/index";
+    }
+    
+    @GetMapping("/{id}")
+    public String show(@PathVariable(name = "id") Integer id, Model model){
+        //  Optional<Pizza> pizzaAttempt = pizzaRepository.findById(id);
+        //  Pizza pizza = pizzaAttempt.get();
+        Pizza pizza = pizzaRepository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "pizzas/show";
     }
 }
